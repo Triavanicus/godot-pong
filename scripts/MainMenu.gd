7693debb5ddfd610a7e722337f19a7b4ec618642
Triavanicus <triavanicus@gmail.com>
2024-01-17 05:37:26 -0600

    Updated ui

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -2,6 +2,7 @@ extends Control
 
 var single_game := preload("res://scenes/game.tscn")
 var multi_game := preload("res://scenes/multi_game.tscn")
+var has_focus = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,3 +24,8 @@ func _on_multi_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _input(event):
+	if event.is_released() && !has_focus: 
+		$VSplitContainer/VBoxContainer/SingleButton.grab_focus()
+		has_focus = !has_focus
